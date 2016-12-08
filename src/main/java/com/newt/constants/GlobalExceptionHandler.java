78facd46aa9c479c6b9f6eabb247cf10b5acbdb0
Manogari G <manogarig@newtglobal.chn.local>
2016-12-08 19:29:55 +0530

    Initial commit

---
+++ src/main/java/com/newt/constants/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.newt.constants;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * An example of an application-specific exception. Defined here for convenience
+ * as we don't have a real domain model or its associated business logic.
+ */
+//@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid Input Provided")
+public class GlobalExceptionHandler extends RuntimeException  {
+	
+
+ /**
+  * Unique ID for Serialized object
+  */
+ private static final long serialVersionUID = -8790211652911971729L;
+
+ public GlobalExceptionHandler(String msg) {
+  super(msg);
+ }
+ 
+ public GlobalExceptionHandler(String msg, Throwable t) {
+  super(msg, t);
+ }
+
+
+}
